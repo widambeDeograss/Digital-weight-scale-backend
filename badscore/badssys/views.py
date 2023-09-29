ca594ac0eb15823cd89ec3a776ad01390ed32bea
DEOGASS WIDAMBE <87471672+widambeDeograss@users.noreply.github.com>
2023-09-29 12:11:14 +0300

    Update views.py

--- badscore/badssys/views.py
+++ badscore/badssys/views.py
@@ -1,3 +1,7 @@
+import decimal
+
+# from BeemAfrica import Authorize
+# from BeemAfrica.SMS import SMS
 from .models import *
 from rest_framework.views import *
 from usermanagement.models import User
@@ -36,7 +40,7 @@ class FarmersView(APIView):
                         )
                     data = {
                         "farmer": {
-                            "id": dat['farmer']['id']
+                            "id": dat['id']
                         },
                         "corporate": {
                             "id": dat['corporate_society']['id'],
@@ -60,5 +64,52 @@ class FarmersView(APIView):
 
 
 class CropSale(APIView):
-    pass
+    permission_classes = [AllowAny]
 
+    @staticmethod
+    def post(request):
+        data = request.data
+        crod_sell_id = data["cropId"]
+        farmer_id = data["farmerId"]
+        quantity_in_kg = data["quantity_in_kg"]
+        farmer = Farmers.objects.get(id=farmer_id)
+        cor_crop = CorporateCrops.objects.get(id=crod_sell_id)
+        crop_price = cor_crop.crop.priceperkg
+        crop_moisture = cor_crop.crop.moisturePercentage
+        print(type(quantity_in_kg))
+        print(type(crop_price))
+        print(type(crop_moisture))
+
+        #take moisture/ 100 multiply by the quantity in kg
+        quantity_with_reduced_moisture = decimal.Decimal(str(quantity_in_kg)) - (decimal.Decimal(str(quantity_in_kg)) * (crop_moisture / 100))
+        total_pay_price = quantity_with_reduced_moisture * crop_price
+        crops_sale = CropSales.objects.create(farmer=farmer, cropSold=cor_crop, quantityInKg=quantity_in_kg, totalPay=total_pay_price)
+        crops_sale.save()
+        print(crops_sale.id)
+
+        data_to_receipt = {
+            "id": crops_sale.id,
+            "cropSold": crops_sale.cropSold.crop.name,
+            "farmer": crops_sale.farmer.farmer.full_name,
+            "saledate": crops_sale.saledate,
+            "quantity_before": quantity_in_kg,
+            "moisturePercentage": crop_moisture,
+            "quantityInKg": crops_sale.quantityInKg,
+            "totalPay": total_pay_price
+        }
+        # message = (f'E-Mzani \n Ndugu {crops_sale.cropSold.crop.name} umeuza { crops_sale.cropSold.crop.name} \n '
+        #            f'{quantity_in_kg}Kgs kwa {total_pay_price}Tsh Tarehe { crops_sale.saledate} \n '
+        #            f'RECEIPT ID {crops_sale.id}')
+        # Authorize('478040a68e5f755d',
+        #           'ZTVkMzUwYWI5NjMwYjM2Zjc0ZTY1ZGQ5ZmQzZWNjNTMwYzRkOTEyYWRlODdhNWIxYmExYmQxOGZkMGNiODdiYg==')
+        #
+        # SMS.send_sms(message, crops_sale.farmer.farmer.phone)
+
+        print(data_to_receipt)
+        return Response(data_to_receipt)
+
+# {
+#     "cropId":"201c7c81-538d-49a4-85af-99325a74280f",
+#     "farmerId":"e8d67685-c567-4e64-be3e-3a6a3eccd143",
+#     "quantity_in_kg":123.3
+# }
